88f54e350dc7ad959fabea22ce202cbeda7b27ab
annikagashi123 <annikagashi18@gmail.com>
2024-12-09 09:41:48 -0800

    kvhif

---
+++ java.js
@@ -0,0 +1,39 @@
+// let stringu1 = "Mirjeta12235467r78";
+// console.log(stringu1.length);
+// let indeksifundit = emri.length - 1 ;
+// console.log(emri[indeksifundit]);
+
+
+// 2.
+let fjala = "abcd";
+//fjala[0] = a;
+//fjala[1] = b;
+//fjala[2] = c;
+//fjala[3] = d;
+// concole.log(fjala.length);
+
+// for (let i = 0; i < fjala.length; i++) {
+    // console.log(fjala[i]);
+    // }
+
+
+
+// 3. 
+// for (let i = 3; i < fjala.length; i++) {
+//     console.log(fjala[i]);
+    
+// }
+
+
+// 4 . 
+
+// fjala[0] = a;
+// fjala[1] = b;
+// fjala[2] = c;
+// fjala[3] = d;\
+
+
+ console.log(fjala.length);
+ for (let i = 0; i < fjala.length; i++) {
+       console.log("fjala ["+ i +"]="  + fjala[i]);
+ }
